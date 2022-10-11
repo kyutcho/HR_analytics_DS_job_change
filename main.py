54236f5c3d1c1c9995e649a0d9339b7c28e785a8
Jayden Cho - HP Omen <kyutcho@umich.edu>
2022-10-10 19:27:12 -0700

    added first MLflow project run

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
 import json
+import tempfile
 
 import mlflow
 import hydra
 from omegaconf import DictConfig
 
-@hydra.main(config_path="config.yaml")
+@hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg: DictConfig) -> None:
     root_path = hydra.utils.get_original_cwd()
 
     print(root_path)
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        run_info = mlflow.run(
+            project_uri,
+            parameters
+        )
 
 
 if __name__ == '__main__':
